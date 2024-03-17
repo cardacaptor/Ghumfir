acbc9eec5ad4fd631ec26aa14edf81fd36763f8d
sjtsh <mastersajat@gmail.com>
2024-03-17 20:10:53 +0545

    working with drf

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from chatbot.models import ChatMessage, MessagePost
+
 # Register your models here.
+
+admin.site.register(ChatMessage)
+admin.site.register(MessagePost)
--- feed/admin.py
+++ feed/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from feed.models.post import Post
+from feed.models.category import Category
+from feed.models.post import Post, PostTag, Tag
+from feed.models.post_action import PostAction
+from feed.models.post_viewed import PostViewed
 # Register your models here.q
 
 
 admin.site.register(Post)
+admin.site.register(PostAction)
+admin.site.register(PostViewed)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(PostTag)
